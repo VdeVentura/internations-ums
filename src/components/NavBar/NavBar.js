cbfeb9a7b656278d03451a41a4e0c3d53f43bc42
Irving Ventura <irvingv8@gmail.com>
2017-09-30 16:49:37 -0400

    -> Made brand clickeable

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,8 +7,10 @@ class NavBar extends Component {
   render() {
     return (
       <div className="nav-bar">
-        <img src={logo} alt="" className="logo" />
-        <div className="brand">Users Management System</div>
+        <NavLink className="brand" exact to="/">
+          <img src={logo} alt="" className="logo" />
+          <div className="text">Users Management System</div>
+        </NavLink>
         <NavLink className="item" exact to="/">
           Dashboard
         </NavLink>
