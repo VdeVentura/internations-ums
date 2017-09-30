6bf2d32bf06fa97dd9adb0b18e41ce787acfc3da
Irving Ventura <irvingv8@gmail.com>
2017-09-30 01:47:49 -0400

    -> Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import { Button } from "semantic-ui-react";
+
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
+}
+
+export default App;
