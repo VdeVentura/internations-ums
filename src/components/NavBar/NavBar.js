1b2c9bc36564e2110b67a14e7f361eb69e9cf512
Irving Ventura <irvingv8@gmail.com>
2017-09-30 04:26:29 -0400

    -> NavBar done

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "./logo.png";
 import "./NavBar.css";
 
 class NavBar extends Component {
   render() {
     return (
       <div className="nav-bar">
+        <img src={logo} alt="" className="logo" />
         <div className="brand">Users Management System</div>
+        <NavLink className="item" exact to="/">
+          Dashboard
+        </NavLink>
+        <NavLink className="item" to="/users">
+          Users
+        </NavLink>
+        <NavLink className="item" to="/groups">
+          Groups
+        </NavLink>
       </div>
     );
   }
