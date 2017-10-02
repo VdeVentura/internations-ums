fa6a28dcc9f0839e6736841dce671ff4ae8d1014
Irving Ventura <irvingv8@gmail.com>
2017-10-02 02:59:14 -0400

    -> Enhanced Modal (close on esc key, close button now accessible by keyboard)

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,17 +1,42 @@
 import React, { Component } from "react";
 import "./Modal.css";
 class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.escListener = this.escListener.bind(this);
+  }
+  escListener(event) {
+    if (event.keyCode === 27) {
+      this.props.hide();
+    }
+  }
+  componentDidMount() {
+    document.addEventListener("keydown", this.escListener, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.escListener, false);
+  }
   render() {
     return (
-      <div className={`modal-wrapper ${this.props.visible ? "visible" : ""}`}>
+      <div
+        onKeyDown={e => {
+          if (e.keyCode === 27) {
+            this.props.hide();
+          }
+        }}
+        ref={modal => {
+          this.focusDiv = modal;
+        }}
+        className={`modal-wrapper ${this.props.visible ? "visible" : ""}`}
+      >
         <div
           className={`overlay ${this.props.visible ? "fade" : ""}`}
           onClick={this.props.hide}
         />
         <div className="modal">
-          <div className="close" onClick={this.props.hide}>
+          <button className="close" onClick={this.props.hide}>
             <i className="fa fa-times" aria-hidden="true" />
-          </div>
+          </button>
           <div className="header">
             <h1>{this.props.header}</h1>
           </div>
